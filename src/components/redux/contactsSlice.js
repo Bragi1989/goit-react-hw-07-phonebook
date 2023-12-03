2da741809c112723a7b5f63734d64caf1e870cc5
Bragi1989 <130233226+Bragi1989@users.noreply.github.com>
2023-12-03 01:09:38 +0100

    1

--- src/components/redux/contactsSlice.js
+++ src/components/redux/contactsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const baseUrl = 'https://656bc56ae1e03bfd572dd2a0.mockapi.io';
+const baseUrl = 'https://656bc56ae1e03bfd572dd2a0.mockapi.io/goit-react-hw-07-phonebook';
 
 export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
   const response = await axios.get(`${baseUrl}/contacts`);
